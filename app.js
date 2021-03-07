89e03befbad7a516de246aebef1fc6c0455f4dbe
igorPae <igor.paevshik@gmail.com>
2021-03-07 18:09:58 +0200

    dsfsd

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDLEWARES
+
+app.use(cors());
+
+app.options('*', cors());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
